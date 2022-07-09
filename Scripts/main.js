aabcf91fde49ce496b3c5da7fa0815c1aaa8813f
Thomas Lagrutta <thomas.lagrutta1@gmail.com>
2022-07-09 18:17:08 -0300

    Dom y eventos

--- Scripts/main.js
+++ Scripts/main.js
@@ -578,87 +578,132 @@
 
 // dia 7
 
-class anteojo {
-    constructor(nombre, precio, tipo, marca) {
-        this.nombre = nombre;
-        this.precio = parseInt(precio);
-        this.tipo = tipo;
-        this.marca = marca;
-    }
-}
+// class anteojo {
+//     constructor(nombre, precio, tipo, marca) {
+//         this.nombre = nombre;
+//         this.precio = parseInt(precio);
+//         this.tipo = tipo;
+//         this.marca = marca;
+//     }
+// }
 
-const sol1 = new anteojo ("Londres", 17500, "Sol", "Vulk");
-const sol2 = new anteojo ("Munich", 17000,"Sol", "Vulk");
-const sol3 = new anteojo ("Bruselas", 16000,"Sol", "Rusty");
-const sol4 = new anteojo ("Praga", 15000,"Sol", "Ossira");
-const sol5 = new anteojo ("Budapest", 15000,"Sol", "Ossira");
-const sol6 = new anteojo ("Seúl", 15000,"Sol", "Rusty");
-const sol7 = new anteojo ("Hong Kong", 14000, "Sol", "Vulk");
-const sol8 = new anteojo ("Tokyo", 13700, "Sol", "Vulk");
-let anteojosSol = [];
+// const sol1 = new anteojo ("Londres", 17500, "Sol", "Vulk");
+// const sol2 = new anteojo ("Munich", 17000,"Sol", "Vulk");
+// const sol3 = new anteojo ("Bruselas", 16000,"Sol", "Rusty");
+// const sol4 = new anteojo ("Praga", 15000,"Sol", "Ossira");
+// const sol5 = new anteojo ("Budapest", 15000,"Sol", "Ossira");
+// const sol6 = new anteojo ("Seúl", 15000,"Sol", "Rusty");
+// const sol7 = new anteojo ("Hong Kong", 14000, "Sol", "Vulk");
+// const sol8 = new anteojo ("Tokyo", 13700, "Sol", "Vulk");
+// let anteojosSol = [];
 
-const leer1 = new anteojo ("Amsterdam",25000,"Leer", "Ossira");
-const leer2 = new anteojo ("Barcelona",22000,"Leer", "Vulk");
-const leer3 = new anteojo ("París",21000,"Leer", "Ossira");
-let anteojosLeer = [];
-
-
-anteojosSol.push(sol1,sol2,sol3,sol4,sol5,sol6,sol7,sol8);
-anteojosLeer.push(leer1,leer2, leer3)
-console.log(anteojosSol);
-console.log(anteojosLeer);
+// const leer1 = new anteojo ("Amsterdam",25000,"Leer", "Ossira");
+// const leer2 = new anteojo ("Barcelona",22000,"Leer", "Vulk");
+// const leer3 = new anteojo ("París",21000,"Leer", "Ossira");
+// let anteojosLeer = [];
 
 
-const vulkSol = anteojosSol.filter((el)=>{
-    return el.marca === "Vulk"
-})
-console.log(vulkSol);
-
-const rustySol = anteojosSol.filter((el)=>{
-    return el.marca === "Rusty"
-})
-console.log(rustySol);
-
-const ossiraSol = anteojosSol.filter((el)=>{
-    return el.marca === "Ossira"
-})
-console.log(ossiraSol);
-
-const vulkLeer = anteojosLeer.filter((el)=>{
-    return el.marca === "Vulk"
-})
-console.log(vulkLeer);
-
-const ossiraLeer = anteojosLeer.filter((el)=>{
-    return el.marca === "Ossira"
-})
-console.log(ossiraLeer);
+// anteojosSol.push(sol1,sol2,sol3,sol4,sol5,sol6,sol7,sol8);
+// anteojosLeer.push(leer1,leer2, leer3)
+// console.log(anteojosSol);
+// console.log(anteojosLeer);
 
 
-const baratos = anteojosSol.slice(3, 8)
-console.log(baratos);
+// const vulkSol = anteojosSol.filter((el)=>{
+//     return el.marca === "Vulk"
+// })
+// console.log(vulkSol);
+
+// const rustySol = anteojosSol.filter((el)=>{
+//     return el.marca === "Rusty"
+// })
+// console.log(rustySol);
+
+// const ossiraSol = anteojosSol.filter((el)=>{
+//     return el.marca === "Ossira"
+// })
+// console.log(ossiraSol);
+
+// const vulkLeer = anteojosLeer.filter((el)=>{
+//     return el.marca === "Vulk"
+// })
+// console.log(vulkLeer);
+
+// const ossiraLeer = anteojosLeer.filter((el)=>{
+//     return el.marca === "Ossira"
+// })
+// console.log(ossiraLeer);
+
+
+// const baratos = anteojosSol.slice(3, 8)
+// console.log(baratos);
 
 
 
-const sumar = function (numeroA, numeroB) {
-    return numeroA+numeroB;
-}
+// const sumar = function (numeroA, numeroB) {
+//     return numeroA+numeroB;
+// }
 
-const descuento = function (resultado, desc1) {
-    return resultado*desc1;
-}
+// const descuento = function (resultado, desc1) {
+//     return resultado*desc1;
+// }
 
-let numeroA = parseInt(prompt ("Ingresa el precio del producto"));
-let numeroB = parseInt(prompt ("Ingresa el precio del otro producto"));
-let resultado = sumar
-let desc1 = parseFloat(0.90)
-let precioF = alert("Tu precio final con descuento es de " + (numeroA+numeroB)*desc1)
+// let numeroA = parseInt(prompt ("Ingresa el precio del producto"));
+// let numeroB = parseInt(prompt ("Ingresa el precio del otro producto"));
+// let resultado = sumar
+// let desc1 = parseFloat(0.90)
+// let precioF = alert("Tu precio final con descuento es de " + (numeroA+numeroB)*desc1)
 
-function mostrar(mensaje) {
-    console.log(mensaje);
-}
+// function mostrar(mensaje) {
+//     console.log(mensaje);
+// }
 
-mostrar(sumar(numeroA, numeroB));
-mostrar(descuento((numeroA+numeroB), desc1));
+// mostrar(sumar(numeroA, numeroB));
+// mostrar(descuento((numeroA+numeroB), desc1));
 
 
+// dia8
+
+// // buscar clases del html por medio de js 
+// const imagenindex = document.getElementsByClassName ("imagenindex");
+// console.log(imagenindex);
+// // buscar por nombre de etiqueta
+// const p = document.getElementsByTagName("p")
+// console.log(p);
+
+// for (const parrafo of p) {
+//     console.log(parrafo);
+// }
+
+// p[0].innerHTML= "cambiazo" // con este podes crear estructuras diferentes del html
+// p[1].innerText= "cambiazo2" // con este solo cambias txt
+
+// crear elementos
+
+// const cont = document.createElement("main")
+// cont.innerHTML= "<h3>Titulo creado dinamicamente</h3>"
+// document.body.append(cont)
+
+// cont.remove()
+
+// template literals
+// concatenar +
+// let plantilla = `ID: ${producto.id} Producto: ${producto.nombre} $${producto.precio}
+
+// dia 9
+
+// btnV = document.querySelector(".btnV"), 
+// p = document.querySelector("#mensaje"),
+// inputIngreso = document.getElementById("ingreso");
+
+// addEventListener
+
+// btnV.addEventListener("Vulk", ()=>{
+//     p.innerText = "Modelos Vulk"
+// })
+
+// on(nombre del evento)
+
+// btnV.onclick = ()=>{
+//     p.innerText = "Modelos Vulk"
+// }
--- Scripts/practica.js
+++ Scripts/practica.js
@@ -1,4 +1,67 @@
-// tarea final 1
+
+// tarea arrays 
+class anteojo {
+    constructor(nombre, precio, tipo, marca) {
+    this.nombre = nombre;
+    this.precio = parseInt(precio);
+    this.tipo = tipo;
+    this.marca = marca;
+    }
+}
+
+const sol1 = new anteojo ("Londres", 17500, "Sol", "Vulk");
+const sol2 = new anteojo ("Munich", 17000,"Sol", "Vulk");
+const sol3 = new anteojo ("Bruselas", 16000,"Sol", "Rusty");
+const sol4 = new anteojo ("Praga", 15000,"Sol", "Ossira");
+const sol5 = new anteojo ("Budapest", 15000,"Sol", "Ossira");
+const sol6 = new anteojo ("Seúl", 15000,"Sol", "Rusty");
+const sol7 = new anteojo ("Hong Kong", 14000, "Sol", "Vulk");
+const sol8 = new anteojo ("Tokyo", 13700, "Sol", "Vulk");
+let anteojosSol = [];
+
+const leer1 = new anteojo ("Amsterdam",25000,"Leer", "Ossira");
+const leer2 = new anteojo ("Barcelona",22000,"Leer", "Vulk");
+const leer3 = new anteojo ("París",21000,"Leer", "Ossira");
+let anteojosLeer = [];
+
+
+anteojosSol.push(sol1,sol2,sol3,sol4,sol5,sol6,sol7,sol8);
+anteojosLeer.push(leer1,leer2, leer3)
+console.log(anteojosSol);
+console.log(anteojosLeer);
+
+
+const vulkSol = anteojosSol.filter((el)=>{
+    return el.marca === "Vulk"
+})
+console.log(vulkSol);
+
+const rustySol = anteojosSol.filter((el)=>{
+    return el.marca === "Rusty"
+})
+console.log(rustySol);
+
+const ossiraSol = anteojosSol.filter((el)=>{
+    return el.marca === "Ossira"
+})
+console.log(ossiraSol);
+
+const vulkLeer = anteojosLeer.filter((el)=>{
+    return el.marca === "Vulk"
+})
+console.log(vulkLeer);
+
+const ossiraLeer = anteojosLeer.filter((el)=>{
+    return el.marca === "Ossira"
+})
+console.log(ossiraLeer);
+
+
+const baratos = anteojosSol.slice(3, 8)
+console.log(baratos);
+
+
+
 const sumar = function (numeroA, numeroB) {
     return numeroA+numeroB;
 }
@@ -7,12 +70,11 @@ const descuento = function (resultado, desc1) {
     return resultado*desc1;
 }
 
-
 let numeroA = parseInt(prompt ("Ingresa el precio del producto"));
 let numeroB = parseInt(prompt ("Ingresa el precio del otro producto"));
-let resultado = sumar
+let resultado = sumar;
 let desc1 = parseFloat(0.90)
-let precioF = alert("Tu precio final es de " + (numeroA+numeroB)*desc1)
+let precioF = alert("Tu precio final con descuento es de " + (numeroA+numeroB)*desc1);
 
 function mostrar(mensaje) {
     console.log(mensaje);
@@ -21,32 +83,10 @@ function mostrar(mensaje) {
 mostrar(sumar(numeroA, numeroB));
 mostrar(descuento((numeroA+numeroB), desc1));
 
-// tarea arrays 
-class anteojo {
-    constructor(nombre, precio, tipo) {
-        this.nombre = nombre;
-        this.precio = parseInt(precio);
-        this.tipo = tipo;
-    }
+let agregar = document.getElementById("prueba")
+agregar.innerText = "Hola"
+
+botonP = document.querySelector(".botonP")
+botonP.onclick = ()=>{
+    agregar.innerText = "Chau"
 }
-
-const sol1 = new anteojo ("Londres", 17500, "Sol");
-const sol2 = new anteojo ("Munich", 17000,"Sol");
-const sol3 = new anteojo ("Bruselas", 16000,"Sol");
-const sol4 = new anteojo ("Praga", 15000,"Sol");
-const sol5 = new anteojo ("Budapest", 15000,"Sol");
-const sol6 = new anteojo ("Seúl", 15000,"Sol");
-const sol7 = new anteojo ("Hong Kong", 14000, "Sol");
-const sol8 = new anteojo ("Tokyo", 13700, "Sol");
-let anteojosSol = [];
-
-const leer1 = new anteojo ("Amsterdam",25000,"Leer");
-const leer2 = new anteojo ("Barcelona",22000,"Leer");
-const leer3 = new anteojo ("París",21000,"Leer");
-let anteojosLeer = [];
-
-
-anteojosSol.push(sol1,sol2,sol3,sol4,sol5,sol6,sol7,sol8);
-anteojosLeer.push(leer1,leer2, leer3)
-console.log(anteojosSol);
-console.log(anteojosLeer);
