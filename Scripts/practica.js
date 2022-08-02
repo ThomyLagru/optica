4c758227c87536a5d807287d4be972b188c33299
Thomas Lagrutta <thomas.lagrutta1@gmail.com>
2022-08-02 16:34:02 -0300

    fetch

--- Scripts/practica.js
+++ Scripts/practica.js
@@ -8,56 +8,29 @@ class anteojo {
     }
 }
 
-const sol1 = new anteojo ("Londres", 17500, "Sol", "Vulk");
-const sol2 = new anteojo ("Munich", 17000,"Sol", "Vulk");
-const sol3 = new anteojo ("Bruselas", 16000,"Sol", "Rusty");
-const sol4 = new anteojo ("Praga", 15000,"Sol", "Ossira");
-const sol5 = new anteojo ("Budapest", 15000,"Sol", "Ossira");
-const sol6 = new anteojo ("Seúl", 15000,"Sol", "Rusty");
-const sol7 = new anteojo ("Hong Kong", 14000, "Sol", "Vulk");
-const sol8 = new anteojo ("Tokyo", 13700, "Sol", "Vulk");
-let anteojosSol = [];
+// const sol1 = new anteojo ("Londres", 17500, "Sol", "Vulk");
+// const sol2 = new anteojo ("Munich", 17000,"Sol", "Vulk");
+// const sol3 = new anteojo ("Bruselas", 16000,"Sol", "Rusty");
+// const sol4 = new anteojo ("Praga", 15000,"Sol", "Ossira");
+// const sol5 = new anteojo ("Budapest", 15000,"Sol", "Ossira");
+// const sol6 = new anteojo ("Seúl", 15000,"Sol", "Rusty");
+// const sol7 = new anteojo ("Hong Kong", 14000, "Sol", "Vulk");
+// const sol8 = new anteojo ("Tokyo", 13700, "Sol", "Vulk");
+// let anteojosSol = [];
 
-const leer1 = new anteojo ("Amsterdam",25000,"Leer", "Ossira");
-const leer2 = new anteojo ("Barcelona",22000,"Leer", "Vulk");
-const leer3 = new anteojo ("París",21000,"Leer", "Ossira");
-let anteojosLeer = [];
-
-
-anteojosSol.push(sol1,sol2,sol3,sol4,sol5,sol6,sol7,sol8);
-anteojosLeer.push(leer1,leer2, leer3)
-console.log(anteojosSol);
-console.log(anteojosLeer);
+// const leer1 = new anteojo ("Amsterdam",25000,"Leer", "Ossira");
+// const leer2 = new anteojo ("Barcelona",22000,"Leer", "Vulk");
+// const leer3 = new anteojo ("París",21000,"Leer", "Ossira");
+// let anteojosLeer = [];
 
 
-const vulkSol = anteojosSol.filter((el)=>{
-    return el.marca === "Vulk"
-})
-console.log(vulkSol);
-
-const rustySol = anteojosSol.filter((el)=>{
-    return el.marca === "Rusty"
-})
-console.log(rustySol);
-
-const ossiraSol = anteojosSol.filter((el)=>{
-    return el.marca === "Ossira"
-})
-console.log(ossiraSol);
-
-const vulkLeer = anteojosLeer.filter((el)=>{
-    return el.marca === "Vulk"
-})
-console.log(vulkLeer);
-
-const ossiraLeer = anteojosLeer.filter((el)=>{
-    return el.marca === "Ossira"
-})
-console.log(ossiraLeer);
+// anteojosSol.push(sol1,sol2,sol3,sol4,sol5,sol6,sol7,sol8);
+// anteojosLeer.push(leer1,leer2, leer3)
+// console.log(anteojosSol);
+// console.log(anteojosLeer);
 
 
-const baratos = anteojosSol.slice(3, 8)
-console.log(baratos);
+
 
 
 
@@ -117,3 +90,31 @@ Swal.fire({
     width: 400,
 
 })
+
+
+const lista = document.querySelector('#listado')
+fetch('../data/productos.json')
+.then(response => response.json())
+.then(data => {
+    data.forEach((anteojo)=>{
+        const li = document.createElement('li')
+        li.innerHTML = 
+        `
+        ${anteojo.nombre}
+        ${anteojo.precio}
+        ${anteojo.img}
+        `
+        lista.append(li)
+    })
+})
+.catch(error => console.log(error))
+
+// const PedirProd = async ()=>{
+//     const respuesta = await fetch('../data/productos.json')
+//     const data= await respuesta.json()
+
+//     console.log(data);
+// }
+// PedirProd()
+
+
